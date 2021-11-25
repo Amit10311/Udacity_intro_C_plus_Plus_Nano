7f0548a7a55f3502391a168d52e53eaf4f85e3be
Amit Kumar <amitkumar10311@gmail.com>
2021-11-25 08:45:56 +0100

    Update main.cpp

--- 2_Foundations/main.cpp
+++ 2_Foundations/main.cpp
@@ -6,3 +6,10 @@ using std::cout;
 int main() {
     cout << "Hello!" << "\n";   
 }
+
+// You can use two forward slashes for single line comments.
+
+/*
+For longer comments, you can enclose the text with an opening
+slash-star and closing star-slash.
+*/ 
