d50e89fb294a83aff53166d741d20a22de126f27
Amit Kumar <amitkumar10311@gmail.com>
2021-11-26 06:06:04 +0100

    Update printing_1d.cpp

--- 2_Foundations/printing_1d.cpp
+++ 2_Foundations/printing_1d.cpp
@@ -4,11 +4,12 @@ using std::vector;
 using std::cout;
 
 int main() {
-    vector<int> a = {0, 1, 2, 3, 4};  // 1D vector 
+    vector<int> a = {0, 1, 2, 3, 4};
     // Add some code here to access and print elements of a.
     
-    cout << a[0]<<"\n";
-    cout << a[1]<<"\n";
-    cout << a[2]<<"\n";
+    for(int i : a)
+        cout << i;
     cout << "\n";
+    
+    cout << a[0]<<"\n"; // 2nd way 
 }
