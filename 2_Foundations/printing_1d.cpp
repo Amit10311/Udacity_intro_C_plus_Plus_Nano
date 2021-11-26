72e98cff76382c132721290debdc59d6967ffdf0
Amit Kumar <amitkumar10311@gmail.com>
2021-11-26 06:05:42 +0100

    Update printing_1d.cpp

--- 2_Foundations/printing_1d.cpp
+++ 2_Foundations/printing_1d.cpp
@@ -5,6 +5,8 @@ using std::cout;
 
 int main() {
     vector<int> a = {0, 1, 2, 3, 4};  // 1D vector 
+    // Add some code here to access and print elements of a.
+    
     cout << a[0]<<"\n";
     cout << a[1]<<"\n";
     cout << a[2]<<"\n";
