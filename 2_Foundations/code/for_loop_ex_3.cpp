44a3c95ea8ac37616747e5c9e452aa862140d90f
Amit Kumar <amitkumar10311@gmail.com>
2021-11-30 01:50:34 +0100

    Create for_loop_ex_3.cpp

---
+++ 2_Foundations/code/for_loop_ex_3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
+
+int main() {
+    // Add your code here.
+    vector<vector<int>> b {{1, 2},
+                           {3, 4},
+                           {5, 6}};
+
+    // Write your double loop here.
+    for(auto v : b) {
+        for(int i : v) {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+}
+
+/* ##  **Challenge**
+
+Write a double range-based for loop that prints all of the entries of the 2D vector `b`. 
+
+
+*/ 
