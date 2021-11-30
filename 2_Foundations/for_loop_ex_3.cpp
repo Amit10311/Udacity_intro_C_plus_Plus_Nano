393b3bf82a606d8fed367852084f458f5b02d89a
Amit Kumar <amitkumar10311@gmail.com>
2021-11-30 01:56:57 +0100

    Update for_loop_ex_3.cpp

--- 2_Foundations/for_loop_ex_3.cpp
+++ 2_Foundations/for_loop_ex_3.cpp
@@ -16,11 +16,20 @@ int main() {
         }
         cout << "\n";
     }
+    cout <<"Length of b vector rows : "<<b.size()<<"\n"; 
+    cout <<"Length of b vector coloumns : "<<b[0].size()<<"\n";  
 }
 
 /* ##  **Challenge**
 
 Write a double range-based for loop that prints all of the entries of the 2D vector `b`. 
 
+*/
 
+/* Ouput 
+1 2 
+3 4 
+5 6 
+Length of b vector rows : 3
+Length of b vector coloumns : 2
 */ 
