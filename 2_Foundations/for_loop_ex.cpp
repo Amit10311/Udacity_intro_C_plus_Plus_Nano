573b1e044a9c3754db6f3bed097a9d876df36ba2
Amit Kumar <amitkumar10311@gmail.com>
2021-11-30 01:27:42 +0100

    Create for_loop_ex.cpp

---
+++ 2_Foundations/for_loop_ex.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+
+int main() {
+    for (int i=-3; i = 10 i++) {
+      cout << i << "\n";
+    }
+    
+}
+
+/* 
+Witing a for loop that prints values from -3 through 10 in the cell below.
+
+
+*/ 
