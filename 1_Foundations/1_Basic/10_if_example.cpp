be41a3895676ac2d3686753aa83f40f301f93677
Amit Kumar <amitkumar10311@gmail.com>
2021-12-02 01:26:28 +0100

    Create 10_if_example.cpp

---
+++ 1_Foundations/1_Basic/10_if_example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using std::cout;
+
+int main() 
+{
+
+    // Set a equal to true here.
+    bool a = false;
+
+    if (a) {
+      cout << "Hooray! You made it into the if statement!" << "\n";
+    }
+}
+
+/* OutPut 
+
+Nothing , condition is false
+
+*/ 
