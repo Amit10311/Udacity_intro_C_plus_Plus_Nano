59b9855a13126da80c337f804c9ece81d7d8a579
Amit Kumar <amitkumar10311@gmail.com>
2021-12-03 02:09:44 +0100

    Create 12_solution_read_board_file.cpp

---
+++ 1_Foundations/1_Basic/12_solution_read_board_file.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::ifstream;
+using std::string;
+using std::vector;
+
+
+void ReadBoardFile(string path) {
+  ifstream myfile (path);
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      cout << line << "\n";
+    }
+  }
+}
+
+int main() {
+  // TODO: Call the ReadBoardFile function here.
+  ReadBoardFile("1.board");
+ 
+}
+
+/*
+The file stream has been created!
+0,1,0,0,0,0,
+0,1,0,0,0,0,
+0,1,0,0,0,0,
+0,1,0,0,0,0,
+0,0,0,0,1,0,
+
+*/
