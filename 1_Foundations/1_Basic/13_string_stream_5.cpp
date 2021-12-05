da799ce8bbd4cf434278df1bc9abc86e976ae6bb
Amit Kumar <amitkumar10311@gmail.com>
2021-12-05 02:12:51 +0100

    Create 13_string_stream_5.cpp

---
+++ 1_Foundations/1_Basic/13_string_stream_5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using std::istringstream;
+using std::string;
+using std::cout;
+
+int main() 
+{
+    string b("1,2,3p");
+
+    istringstream my_stream(b);
+
+    char c;
+    int n;
+
+    while (my_stream >> n >> c) {
+      cout << "That stream was successful:" << n << " " << c << "\n";
+    }
+    cout << "The stream has failed." << "\n";
+}
+
+/* OUTPUT
+
+That stream was successful:1 ,
+That stream was successful:2 ,
+That stream was successful:3 p
+The stream has failed.
+
+*/ 
