7c90b64bdf6e0d257f13faddbe3129946f936c0f
Amit Kumar <amitkumar10311@gmail.com>
2022-05-02 15:39:40 +0200

    Update 12_solution_read_board_file.cpp

--- 1_Foundations/1_Basic/12_solution_read_board_file.cpp
+++ 1_Foundations/1_Basic/12_solution_read_board_file.cpp
@@ -18,10 +18,21 @@ void ReadBoardFile(string path) {
   }
 }
 
+// PrintBoard not used in this exercise
+void PrintBoard(const vector<vector<int>> board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[i].size(); j++) {
+      cout << board[i][j];
+    }
+    cout << "\n";
+  }
+}
+
 int main() {
   // TODO: Call the ReadBoardFile function here.
   ReadBoardFile("1.board");
- 
+  // Leave the following line commented out.
+  //PrintBoard(board);
 }
 
 /*
