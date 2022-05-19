f1f51c14a6ecbb47a154a19be980a3745987de33
Amit Kumar <amitkumar10311@gmail.com>
2022-05-19 15:20:53 +0200

    Update 2_memory_example_4.cpp

--- 1_Foundations/3_Writing Multifile Programs/2_memory_example_4.cpp
+++ 1_Foundations/3_Writing Multifile Programs/2_memory_example_4.cpp
@@ -14,15 +14,18 @@ int main() {
     i = 7;
     cout << "The new value of the variable i is                     : " << i << "\n";
     cout << "The value of the variable pointed to by pointer_to_i is: " << *pointer_to_i << "\n";
+    cout << "The variable pointer_to_i is: " << pointer_to_i << "\n";
 }
-
 
   /*  Getting an Object Back from a Pointer Address
   
 The address of i is          : 0x7ffff17b7a0c
 The variable pointer_to_i is : 0x7ffff17b7a0c
+
 The new value of the variable i is                     : 7
 The value of the variable pointed to by pointer_to_i is: 7
+
+The variable pointer_to_i is: 0x7ffcbfad5a0c
 
   */ 
 
