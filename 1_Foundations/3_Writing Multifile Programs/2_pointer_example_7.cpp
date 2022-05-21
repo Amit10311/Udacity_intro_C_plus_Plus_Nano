2f8e11a4b8687582e249066a2c942e1ad6736995
Amit Kumar <amitkumar10311@gmail.com>
2022-05-21 04:09:11 +0200

    Update 2_pointer_example_7.cpp

--- 1_Foundations/3_Writing Multifile Programs/2_pointer_example_7.cpp
+++ 1_Foundations/3_Writing Multifile Programs/2_pointer_example_7.cpp
@@ -6,6 +6,7 @@ int* AddOne(int& j)
     // Increment the referenced int and return the
     // address of j.
     j++;
+    cout << "The memory address of j : " << &j << "\n";
     return &j;
 }
 
@@ -17,13 +18,20 @@ int main()
     // Declare a pointer and initialize to the value
     // returned by AddOne:
     int* my_pointer = AddOne(i);
+    cout << "The memory address of i : " << &i << "\n";
+    
     cout << "The value of i is now: " << i << "\n";
     cout << "The value of the int pointed to by my_pointer is: " << *my_pointer << "\n";
 }
 
+
   /*  Returning a Pointer from a Function
   
 The value of i is: 1
+
+The memory address of j : 0x7ffe0f26896c
+The memory address of i : 0x7ffe0f26896c
+
 The value of i is now: 2
 The value of the int pointed to by my_pointer is: 2
 
