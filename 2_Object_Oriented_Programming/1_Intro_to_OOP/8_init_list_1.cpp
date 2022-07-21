99c1be3d0b5cf29d76a3089876997aa6c139e4a4
Amit Kumar <amitkumar10311@gmail.com>
2022-07-21 16:00:46 +0200

    Create 8_init_list_1.cpp

---
+++ 2_Object_Oriented_Programming/1_Intro_to_OOP/8_init_list_1.cpp
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <string>
+#include <iostream>
+
+// TODO: Define structure Person
+struct Person {
+  // TODO: Define a public constructor with an initialization list
+  Person(std::string name) : name(name) {}
+  // TODO: Define a public member variable: name
+  std::string name;  
+};
+
+// Test
+int main() {
+  Person alice("Alice");
+  Person bob("Bob");
+  assert(alice.name != bob.name);
+  std::cout<< alice.name <<"\n";
+}
+
+
+
+
+  /*  Initializer Lists
+  
+Alice
+
+  */ 
